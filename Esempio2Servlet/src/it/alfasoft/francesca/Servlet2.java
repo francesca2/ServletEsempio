1ba33ecfac314db6a8b4c6e7202351a6c02a394f
francesca2 <francesca.giordani88@gmail.com>
2016-10-18 10:28:42 +0200

    Aggiunto secondo servlet

--- Esempio2Servlet/src/it/alfasoft/francesca/Servlet2.java
+++ Esempio2Servlet/src/it/alfasoft/francesca/Servlet2.java
@@ -3,56 +3,36 @@ package it.alfasoft.francesca;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Servlet1
+ * Servlet implementation class Servlet2
  */
-@WebServlet(urlPatterns = { "/ServletRedirect" })
-public class Servlet1 extends HttpServlet {
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		response.setContentType("text/html");
 		String username=request.getParameter("username");
 		String password=request.getParameter("password");
 		
-		RequestDispatcher rds= request.getRequestDispatcher("Servlet2");
-		if(password.equals("123")){
-
-//			response.sendRedirect("benvenuto.html");
-			rds.forward(request, response);
-			
-		}else{
-			response.sendRedirect("login.html");
-		}
+		PrintWriter writer=response.getWriter();
+		writer.println("Benvenuto " + username);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		String username=request.getParameter("username");
-		String password=request.getParameter("password");
-		
-		PrintWriter writer=response.getWriter();
-		if(password.equals("123")){
-			response.sendRedirect("benvenuto.html");;
-		}else{
-			response.sendRedirect("login.html");
-		}
+		// TODO Auto-generated method stub
 	}
 
 }
