5c276ea41778b9dbca1208454a3fc6d3ccff7f39
francesca2 <francesca.giordani88@gmail.com>
2016-10-18 09:35:17 +0200

    Esempio due pagine html

---
+++ Esempio2Servlet/src/it/alfasoft/francesca/Servlet1.java
@@ -0,0 +1,43 @@
+package it.alfasoft.francesca;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		response.setContentType("text/html");
+		
+		PrintWriter writer=response.getWriter();
+		if(password.equals("123")){
+			response.sendRedirect("benvenuto.html");;
+		}else{
+			response.sendRedirect("login.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
